6ac3937a2c9c4647cfd57faa7fec40b0e398d552
Ravi Rajput <nravisingh70@gmail.com>
2025-02-02 23:04:41 +0530

    Leetcode

---
+++ Leetcode/RomanToInteger.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int currValue = 0;
+
+            switch (s[i]) {
+                case 'I': currValue = 1; break;
+                case 'V': currValue = 5; break;
+                case 'X': currValue = 10; break;
+                case 'L': currValue = 50; break;
+                case 'C': currValue = 100; break;
+                case 'D': currValue = 500; break;
+                case 'M': currValue = 1000; break;
+            }
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+};
