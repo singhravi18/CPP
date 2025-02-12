6d1809e6cb96121be663a208d242193b0db15a0d
Ravi Rajput <nravisingh70@gmail.com>
2025-02-12 21:52:04 +0530

    Leetcode

---
+++ Leetcode/reverseBit.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        uint32_t reverseBits(uint32_t n) {
+            uint32_t result = 0;
+            for (int i = 0; i < 32; i++) {
+                result = (result << 1) | (n & 1);
+                n >>= 1;
+            }
+            return result;
+        }
+    };
+    
