59c9bef2aa3364fcaa44f18d6f0a5c67589c2069
Ravi Rajput <nravisingh70@gmail.com>
2025-02-16 21:15:59 +0530

    Leetcode

---
+++ Leetcode/isomorphicStrings.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        bool isIsomorphic(string s, string t) {
+            int m1[256] = {0}, m2[256] = {0};
+            for (int i = 0; i < s.length(); i++) {
+                if (m1[s[i]] != m2[t[i]]) return false;
+                m1[s[i]] = i + 1;
+                m2[t[i]] = i + 1;
+            }
+            return true;
+        }
+    };
+    
