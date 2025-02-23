d69cbbd26119933005aa373edca0130890c68edd
Ravi Rajput <nravisingh70@gmail.com>
2025-02-23 15:38:01 +0530

    Dynamicarray

---
+++ ReferenceVariable/Dynamicarray.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+int getSum (int *arr , int n ){    // The array can also be passed like this (int arr[])
+    int sum = 0 ;
+    for (int i = 0 ; i < n ; i ++){
+        sum = sum + arr[i] ;
+    }
+    return sum;
+}
+int main (){
+
+    int n ;
+    cin >> n ;
+
+    int *arr = new int [n];
+
+    for (int i = 0 ; i < n ; i ++){
+        cin >> arr[i];
+    }
+
+    int ans = getSum (arr , n);
+
+    cout << "the answer is " << ans << endl;
+
+}
