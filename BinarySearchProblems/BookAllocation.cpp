938439963193e120ed7b08bef1597d39e34b4670
Ravi Rajput <nravisingh70@gmail.com>
2025-02-28 17:59:48 +0530

    BinarySearchProblems

---
+++ BinarySearchProblems/BookAllocation.cpp
@@ -0,0 +1,57 @@
+# include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool isValid (vector <int> arr , int n , int m , int maxPagesAllowed){
+    int student = 1 , pages = 0 ;
+
+    for (int i = 0 ; i < n ; i ++){
+        if (arr[i] > maxPagesAllowed){
+            return false;
+        }
+
+        if (pages + arr [i] <= maxPagesAllowed){
+            pages += arr[i];
+        }
+        else {
+            student++;
+            pages = arr[i];
+
+            if (student > m){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+int allocateBooks (vector <int> &arr , int n , int m ){
+    int start = *max_element(arr.begin(), arr.end()); 
+    int end = accumulate (arr.begin(), arr.end() , 0);
+
+    if (m > n){
+        return -1 ;
+    }
+
+    int maxPagesAllowed = start + (end - start)/2;
+    int ans = -1 ;
+
+    if (isValid (arr , n , m , maxPagesAllowed)){
+        ans = maxPagesAllowed;
+        end = maxPagesAllowed - 1;
+    }
+    else {
+        start = maxPagesAllowed + 1 ;
+    }
+    return ans;
+}
+
+int main (){
+    vector <int> arr = {2 , 1 , 3 , 4};
+    int n = 4 ;
+    int m = 2;
+
+    int result = allocateBooks (arr , 4 , 2);
+    cout << result;
+}
